831bcde3ee527d58af8ed1efcff78bbd46996b27
Kylie Vento <kvent@uic.edu>
2025-09-17 19:33:27 -0500

    Step 4

---
+++ WEEKLYEXERCISE4.R
@@ -0,0 +1,8 @@
+# WEEKLYEXERCISE4.R
+# install.packages("usethis")   # run once if not installed
+library(usethis)
+
+default_branch = git_default_branch()
+default_branch
+
+# Default branch comes back as "main"
